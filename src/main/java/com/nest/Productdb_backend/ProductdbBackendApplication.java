14b9ca65318c53db733cded253e4e668c7e956ac
fasilmsageer <fasilmsageer@gmail.com>
2022-12-06 15:27:11 +0530

    first

---
+++ src/main/java/com/nest/Productdb_backend/ProductdbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nest.Productdb_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductdbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductdbBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nest/Productdb_backend/ProductdbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.Productdb_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductdbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
